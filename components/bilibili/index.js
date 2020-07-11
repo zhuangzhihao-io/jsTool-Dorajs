dbfb6e6fdb814d52a2bb453aebc95f7382f22aa9
zhihaofans <zhihaofans@hotmail.com>
2020-07-11 16:41:05 +0800

    Update

--- components/bilibili/index.js
+++ components/bilibili/index.js
@@ -6,7 +6,19 @@ module.exports = {
       style: 'category'
     }, {
       title: 'vtb/vup直播间信息',
-      route: $route('vtbs.moe/liveroom_info')
+      /* route: $route('vtbs.moe/liveroom_info') */
+      onClick: () => {
+        let uid = await $input.prompt({
+          title: '输入用户id',
+          hint: '不是直播间id',
+          value: ''
+        })
+        if (uid) {
+          $router.to($route('vtbs.moe/liveroom_info', { uid: uid }))
+        } else {
+          $ui.toast(`错误，uid不能为空`)
+        }
+      }
     }]
   }
 }
--- components/bilibili/vtbs.moe/liveroom_info.js
+++ components/bilibili/vtbs.moe/liveroom_info.js
@@ -1,13 +1,33 @@
 module.exports = {
-    type: 'list',
-    async fetch({ args, page }) {
-      return [{
-        title: '直播',
-        style: 'category'
-      }, {
-        title: 'vtb/vup直播间信息',
-        route: $route('vtbs.moe/liveroom_info')
-      }]
+  type: 'list',
+  async fetch({ args, page }) {
+    let resp = await $http.get(`https://api.vtbs.moe/v1/detail/${args.uid}`)
+    let list = resp.data.data
+    let items = list.map(data => ({
+      id: data.id,
+      time: data.publishDate,
+      title: data.title,
+      style: 'article',
+      link: data.mobileUrl,
+      author: {
+        name: data.authorName
+      },
+      summary: data.summary,
+      route: $route('@article', {
+        id: data.id,
+        content: {
+          url: data.mobileUrl
+        }
+      })
+    }))
+    let nextPage = null
+    if (list.length > 0) {
+      let publishDate = list[list.length - 1].publishDate
+      nextPage = Date.parse(publishDate)
+    }
+    return {
+      nextPage: nextPage,
+      items: items
     }
   }
-  
+}
